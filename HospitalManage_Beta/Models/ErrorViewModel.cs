6de8f2cf61d1cd4c7ce7fdd7cb1e8f2b26f62292
huyluuit <huy.luu.it@gmail.com>
2019-01-12 16:15:39 +0700

    first commit

---
+++ HospitalManage_Beta/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace HospitalManage_Beta.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
---
+++ HospitalManage_Beta/wwwroot/js/FireBaseScript.js
@@ -0,0 +1,23 @@
+﻿// Initialize Firebase
+var config = {
+    apiKey: "AIzaSyDMr9v-E8CDnJ9EkLDy_GpfIJTVBbcmOYc",
+    authDomain: "hospital-b466b.firebaseapp.com",
+    databaseURL: "https://hospital-b466b.firebaseio.com",
+    projectId: "hospital-b466b",
+    storageBucket: "hospital-b466b.appspot.com",
+    messagingSenderId: "433236587141"
+};
+firebase.initializeApp(config);
+
+var test = firebase.database().ref().child('test');
+var databasetest = document.getElementById('databasetesting');
+
+test.on('value', snap => databasetest.innerText = snap.val());
+
+function WriteNewData(key, object) {
+    firebase.database().ref(key).set(object);
+}
+
+function UpdateData(key, object) {
+
+}
